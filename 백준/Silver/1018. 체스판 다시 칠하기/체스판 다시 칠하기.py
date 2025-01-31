aa5c6af6f758e6f236e27e9fba8ad241576568d3
Choung, Hyunseo <98367152+deveronica@users.noreply.github.com>
2025-01-31 19:31:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,38 @@
+#  **************************************************************************  #
+#                                                                              #
+#                                                       :::    :::    :::      #
+#    Problem Number: 1018                              :+:    :+:      :+:     #
+#                                                     +:+    +:+        +:+    #
+#    By: deveronica <boj.kr/u/deveronica>            +#+    +#+          +#+   #
+#                                                   +#+      +#+        +#+    #
+#    https://boj.kr/1018                           #+#        #+#      #+#     #
+#    Solved: 2025/01/31 17:55:17 by deveronica    ###          ###   ##.kr     #
+#                                                                              #
+#  **************************************************************************  #
+N, M = map(int, input().split())
+nums = [[1 if k == "W" else -1 for k in list(input())] for _ in range(N)]
+
+def calc(x, y):
+    ans1 = 0
+    ans2 = 0
+    for idx, num in enumerate(nums[x:x+8]):
+        if idx % 2 == 0:
+            ans1 += sum(num[y:y+8:2])
+            ans2 += sum(num[y+1:y+8:2])
+        elif idx % 2 != 0:
+            ans1 += sum(num[y+1:y+8:2])
+            ans2 += sum(num[y:y+8:2])
+    case1 = abs(32 - ans1) / 2 + abs(-32 - ans2) / 2
+    case2 = abs(-32 - ans1) / 2 + abs(32 - ans2) / 2
+    return min(case1, case2)
+
+a, b = N - 8, M - 8
+
+best = 32
+for i in range(a + 1):
+    for j in range(b + 1):
+        new = calc(i, j)
+        if new < best:
+            best = new
+
+print(int(best))
